d70f0d0e61182199ae0b4c3123e346ebf352dab0
Sathapon Patanakuha <sathapon@gmail.com>
2023-04-15 13:02:43 +0900

    add daisy ui css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'tangible-primary': '#015e82',
       },
-    },
+    }
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")]
+};
